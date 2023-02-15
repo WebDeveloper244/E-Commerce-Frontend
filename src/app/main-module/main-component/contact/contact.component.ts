db5e89ccdfa1d5980226dc1bc6ee97493b75504b
WebDevolper1122 <sb2137842@gmail.com>
2023-02-15 16:55:12 +0500

    commit

--- src/app/main-module/main-component/contact/contact.component.ts
+++ src/app/main-module/main-component/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder, FormControl } from '@angular/forms';
+import { FormGroup,FormBuilder, FormControl, Validators } from '@angular/forms';
 import { ProductApiService } from 'src/app/shared-service/product-api/product-api.service';
 
 @Component({
@@ -13,15 +13,16 @@ export class ContactComponent implements OnInit {
   constructor(
     private readonly FormBuilder:FormBuilder,
     private readonly emailService:ProductApiService
+    p
   ) { 
     this.emailFormInitialization()
   }
 
   emailFormInitialization(){
     this.emailForm  =  this.FormBuilder.group({
-      firstName:new FormControl(''),
-      email: new FormControl(''),
-      message: new FormControl('')
+      firstName: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(35),Validators.pattern(/^[A-Za-z\s]*$/)]),
+      email: new FormControl('',[Validators.required,Validators.email]),
+      message: new FormControl('',[Validators.required])
     })
   }
 
