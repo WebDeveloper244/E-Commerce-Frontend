f555fd69c664878468e2e089bb1687e64791e62a
WebDevolper1122 <sb2137842@gmail.com>
2023-02-17 22:45:26 +0500

    commit

--- src/app/main-module/main-module.module.ts
+++ src/app/main-module/main-module.module.ts
@@ -1,7 +1,6 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MainModuleRoutingModule } from './main-module-routing.module';
 import { MainModuleComponent } from './main-module.component';
 import { MainComponentComponent } from './main-component/main-component.component';
